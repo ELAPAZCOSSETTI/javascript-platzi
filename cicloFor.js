69573c04f2d3d594865ab3f0d2d288853708b032
ELAPAZCOSSETTI <sk8emi95lp@gmail.com>
2025-11-04 20:51:05 -0300

    forIterationList

---
+++ cicloFor.js
@@ -0,0 +1,11 @@
+// for (variable; condicion; incremento){
+//     codigo a ejecutar
+// }
+
+
+
+let list = ["Estudias", "Trabajas", "duermes", "Vuelve a empezar"];
+
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
+}
