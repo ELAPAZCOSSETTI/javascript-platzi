efb39b9413b1b8db8ae53fb642a5062b87399571
ELAPAZCOSSETTI <sk8emi95lp@gmail.com>
2025-11-03 23:13:53 -0300

    condicionales

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+let nombre = "esteban"
+
+if (nombre === "Franco"){
+    console.log("Hola franco");
+} else if (nombre === "Emiliano"){
+    console.log("hola Emiliano");
+} else {
+    console.log("No estas en la lista de invitados");
+}
