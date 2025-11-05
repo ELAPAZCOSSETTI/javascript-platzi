a56235d023f465aa72716fdf0098d9844d52b403
ELAPAZCOSSETTI <sk8emi95lp@gmail.com>
2025-11-04 21:25:26 -0300

    forOfIteration

---
+++ cicloForOf.js
@@ -0,0 +1,12 @@
+// for of arrays, strings [algo]
+// for (variable of objeto){
+//     codigo a ejectuar
+// }
+// Iteracion para arrays
+ 
+
+let canasta = ["manzana", "banana", "naranja", "pera"];
+
+for (fruta of canasta){
+    console.log(fruta);
+}
